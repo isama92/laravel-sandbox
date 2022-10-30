57a27a1f16fa57eee1336b8bf0e776ec53677e6e
Stefano Borzoni <isama92@gmail.com>
2022-10-30 18:15:46 +0100

    benchmark

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('benchmark', function() {
+    \Illuminate\Support\Benchmark::dd([function() {
+        for($i = 0; $i < 1000000; $i++) {
+            echo $i * 2;
+        }
+    }, function() {
+        foreach(range(0, 999999) as $i) {
+            echo $i * 2;
+        }
+    }]);
+});
